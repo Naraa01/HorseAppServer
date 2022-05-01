1dc12e39e755b87530c56a7c9d7e0498a7e2a78c
Naraa01 <naraam0910@gmail.com>
2022-05-01 22:16:28 +0800

    hello may

---
+++ models/Mon/Comment.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  createUser: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  horse: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Horse",
+  },
+  comment: {
+    type: String,
+    // unique: true,
+    validate: {
+      len: { args: [1, 450], msg: "Name-iin too baga eswel ih baina" },
+    },
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
