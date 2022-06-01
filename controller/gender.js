d3d0029d34f3addb6b29a15aa0a998108f8ebb65
Naraa01 <naraam0910@gmail.com>
2022-06-01 23:08:43 +0800

    hello world

--- controller/gender.js
+++ controller/gender.js
@@ -4,7 +4,9 @@ const asyncHandler = require("express-async-handler");
 
 exports.getGenders = asyncHandler(async (req, res, next) => {
   const select = req.query.select;
-  const genders = await Gender.find(req.query, select);
+  const genders = await Gender.find(req.query, select).sort({
+    sort: "asc",
+  });
 
   res.status(200).json({
     success: true,
--- models/Gender.js
+++ models/Gender.js
@@ -1,32 +1,38 @@
 const mongoose = require("mongoose");
 
-const GenderSchema = new mongoose.Schema({
-  // userId: {
-  //   type: String,
-  // },
-  name: {
-    type: String,
-    unique: true,
-    // validate: {
-    //   len: { args: [2, 75], msg: "Name-iin too baga eswel ih baina" },
+const GenderSchema = new mongoose.Schema(
+  {
+    // userId: {
+    //   type: String,
     // },
+    name: {
+      type: String,
+      unique: true,
+      // validate: {
+      //   len: { args: [2, 75], msg: "Name-iin too baga eswel ih baina" },
+      // },
+    },
+    age: {
+      type: String,
+    },
+    horseId: {
+      type: String,
+    },
+    photo: {
+      type: String,
+      default: "no-photo.png",
+    },
+    origin: {
+      type: String,
+    },
+    pedigree: {
+      type: String,
+    },
+    sort: {
+      type: String,
+    },
   },
-  age: {
-    type: String,
-  },
-  horseId: {
-    type: String,
-  },
-  photo: {
-    type: String,
-    default: "no-photo.png",
-  },
-  origin: {
-    type: String,
-  },
-  pedigree: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Gender", GenderSchema);
